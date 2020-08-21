956c44045fa4c7cdf70024d56a8351eec0968b07
Leonardo lima <57497823+LeeoLima@users.noreply.github.com>
2020-08-21 15:08:36 -0300

    Update Votacao.java

--- aula 10/Votacao.java
+++ aula 10/Votacao.java
@@ -6,23 +6,23 @@ import java.text.DecimalFormat;
 public class Votacao {
 
 	public static void main(String[] args) {
-		//variável de apoio para votar novamente
+		//variÃ¡vel de apoio para votar novamente
 		char novoVoto = 's';
-		//variáveis
+		//variÃ¡veis
 		double candidato;
 		//objetos
 		Scanner teclado = new Scanner (System.in);
 		DecimalFormat formatador = new DecimalFormat("#0");
 		//entrada
 		do {
-		System.out.println("VOTAÇÃO PARA PREFEITO");
+		System.out.println("VOTAÃ‡ÃƒO PARA PREFEITO");
 		System.out.println("");
-		System.out.println("Para o canditado nº 1 digite o número 11111");
-		System.out.println("Para o canditado nº 2 digite o número 22222");
-		System.out.println("Para o canditado nº3 digite o nímero 33333");
+		System.out.println("Para o canditado nÂº 1 digite o nÃºmero 11111");
+		System.out.println("Para o canditado nÂº 2 digite o nÃºmero 22222");
+		System.out.println("Para o canditado nÂº 3 digite o nÃ­mero 33333");
 		System.out.println("Para o o seu voto ser em branco digite o 4");
 		System.out.println("");
-		System.out.println("Digite o número do seu canditado:");
+		System.out.println("Digite o nÃºmero do seu canditado:");
 		candidato = teclado.nextDouble();
 		System.out.println("");
 		System.out.println("Seu candidato selecionado foi o " + formatador.format(candidato));
@@ -31,14 +31,14 @@ public class Votacao {
 		} while (novoVoto == 'n');
 		//saida
 		System.out.println("");
-		System.out.println("PARABÉNS, VOTO CONCLUIDO!" );
+		System.out.println("PARABÃ‰NS, VOTO CONCLUIDO!" );
 		//estrutura de controle condicional
 		if (candidato <=11111 && candidato >11110) {
-			System.out.println("---- CANDIDADO Nº 1 ----");
+			System.out.println("---- CANDIDADO NÂº 1 ----");
 		} else if (candidato <=22222 && candidato >22220) {
-			System.out.println("---- CANDIDADO Nº 2 ----");
+			System.out.println("---- CANDIDADO NÂº 2 ----");
 		} else if ( candidato <=33333 && candidato >33330) {
-			System.out.println("---- CANDIDADO Nº 3 ----");
+			System.out.println("---- CANDIDADO NÂº 3 ----");
 		} else if (candidato <=4 && candidato >3) {
 			System.out.println("---- VOTO EM BRANCO ----");
 		} else {
